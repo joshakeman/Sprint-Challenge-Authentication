1292befb52b38d83a4e6b6ab989a8aea4f52e124
Josh Akeman <joshakeman@gmail.com>
2019-05-10 11:21:22 -0400

    register route working

--- api/server.js
+++ api/server.js
@@ -10,6 +10,10 @@ server.use(helmet());
 server.use(cors());
 server.use(express.json());
 
+server.get('/', (req, res) => {
+    res.send("It's alive!");
+  });
+
 configureRoutes(server);
 
 module.exports = server;
--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,6 @@
 const axios = require('axios');
+const db = require('../database/dbConfig')
+const bcrypt = require('bcryptjs')
 
 const { authenticate } = require('../auth/authenticate');
 
@@ -10,6 +12,18 @@ module.exports = server => {
 
 function register(req, res) {
   // implement user registration
+  let user = req.body
+  const hash = bcrypt.hashSync(user.password, 12)
+  user.password = hash
+  db('users')
+    .insert(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+
 }
 
 function login(req, res) {
